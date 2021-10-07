a18cf092ce097c8e06953af85d9b95c7c0edd921
Gongcu <knmy0101@naver.com>
2021-10-07 20:12:25 +0900

    update: quick sort

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -6,23 +6,31 @@ public class QuickSort {
     }
 
     //피봇을 기준으로 좌측은 작은 값, 우측은 큰 값이 되게 배치
-    static void quickSort(int[] arr, int left, int right){
-        if(left < right){
-            int i = left;
-            int j= right;
-            int pivot = arr[(left+right)/2];
-
-            while(i<j){
-                while(pivot<arr[j]){j--;}
-                while(i<j && pivot>arr[i]){i++;}
-
-                swap(arr, i, j);
-            }
-
-            quickSort(arr, left, i-1);
-            quickSort(arr, i+1, right);
-        }
-    }
+    public static void quickSort(int[] arr ,int left, int right){
+        if(left< right){
+             int index = partition(arr, left, right);
+             quickSort(arr, left, index);
+             quickSort(arr, index+1, right);
+         }
+     }
+     
+     
+     
+     public static int partition(int[] arr, int left, int right){
+         int pivot = arr[(left+right)/2];
+     
+         while(left<=right){
+             while(arr[left] < pivot) {left++;}
+             while(arr[right] > pivot) {right--;}
+     
+             if(left <= right)
+                 swap(arr,left,right);
+                 left++;
+                 right--;
+         }
+             
+         return left;
+     }
 
     static void swap(int[] arr, int i, int j){
         int temp = arr[i];
